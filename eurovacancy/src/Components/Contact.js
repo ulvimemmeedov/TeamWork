0b05fce30194719fa28303d80d02b53f31432bf3
Ulvimemmeedov <onezerolord@gmail.com>
2021-01-28 15:40:07 +0400

    test

---
+++ eurovacancy/src/Components/Contact.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+import Header from './Header'
+import Footer from './Footer'
+import Bootstrap from 'react-bootstrap'
+export default class Contact extends Component {
+    render() {
+        return (
+            <div>
+                <Header />
+                <div>HEllo wordl</div>
+                <Footer/>
+            </div>
+        )
+    }
+}
